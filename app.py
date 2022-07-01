459aa49271cfe9f8a8d1bf76a3fa997bda887f99
abhishekarrk <abhishek.pandravada@arrkgroup.com>
2022-07-01 11:07:17 +0530

    Restored to SQLite as Postgres is not working

--- app.py
+++ app.py
@@ -11,8 +11,7 @@ from resources.items import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-db_url = os.environ.get('DATABASE_URL_NEW')
-app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("://", "ql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL_NEW','sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
